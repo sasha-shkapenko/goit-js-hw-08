922c7756b05b861c1a25e026891ac237785008c2
Sasha Shkapenko <sasha.shkapenko@gmail.com>
2022-04-05 12:47:38 +0200

    fixes bugs

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,15 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+const KEY = "videoplayer-current-time";
 const player = new Player('vimeo-player');
 
 player.on('timeupdate', throttle((data) =>
-  localStorage.setItem("videoplayer-current-time", data.seconds), 1000)
+  localStorage.setItem(KEY, data.seconds), 1000)
 );
 
-const savedTime = localStorage.getItem("videoplayer-current-time");
-player.setCurrentTime(savedTime);
+const savedKey = localStorage.getItem(KEY);
+
+if (savedKey) {
+  player.setCurrentTime(savedTime);
+}
